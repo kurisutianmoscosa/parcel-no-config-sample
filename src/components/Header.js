ee8c2602e2d97799e55be20af2c12129846c2b0a
kurisutian.moscosa <moscosa.tian@gmail.com>
2019-08-01 16:54:05 +0800

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,17 @@
 import React from 'react';
 
-
-export class Header extends React.Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand">
-                <div className="container">
-                    <div className="navbar-header">
-                        <div className="nav navbar-nav">
-                            <li>
-                                <a href="#">Home</a>
-                            </li>
-                        </div>
+export const Header = (props) => {
+    return (
+        <nav className="navbar navbar-expand">
+            <div className="container">
+                <div className="navbar-header">
+                    <div className="nav navbar-nav">
+                        <li>
+                            <a href="#">{props.homeLink}</a>
+                        </li>
                     </div>
                 </div>
-            </nav>
-        );
-    }
+            </div>
+        </nav>
+    );
 };
