e81b033643390dfa3cf3a3fd55ee70a66e9e423e
kurisutian.moscosa <moscosa.tian@gmail.com>
2019-08-02 15:02:33 +0800

    Added text input

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ export class Home extends React.Component {
         this.state = {
             age: props.initialAge,
             status: 0,
+            homeLink: props.initialLinkName
         };
     }
 
@@ -16,6 +17,16 @@ export class Home extends React.Component {
             age: this.state.age += 3
        });
     };
+
+    onChangeLink() {
+        this.props.changeLink(this.state.homeLink);
+    }; 
+
+    onHandleChange(event) {
+        this.setState({
+            homeLink: event.target.value
+        });
+    };
     
     render() {
         return (
@@ -23,12 +34,22 @@ export class Home extends React.Component {
                 <p>Hello {this.props.name}, your age is {this.state.age}</p>
                 <hr/>
                 <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
+                <hr/>
+                <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+                <hr/>
+                <input 
+                    type="text" 
+                    value={this.state.homeLink} 
+                    onChange={(event) => this.onHandleChange(event)}/>
+                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Link!</button>
             </div>
-        );
+        ); 
     }
 }
 
 Home.propTypes  = {
     name: PropTypes.string,
     age: PropTypes.number,
+    greet: PropTypes.func,
+    initialLinkName: PropTypes.string,
 };
